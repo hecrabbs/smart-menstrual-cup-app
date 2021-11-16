1e77661d09754ae6d789f75ef2c777a8d17f5958
unknown <hecrabbs@gmail.com>
2021-11-15 21:48:40 -0800

    outline

--- src/App.js
+++ src/App.js
@@ -1,22 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './Style.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container fluid>
+          <Row>
+            <Col>
+              <Row>
+                <Col className="DashboardItem">
+                  Calendar
+                </Col>
+              </Row>
+              <Row>
+                <Col className="DashboardItem">
+                  Data
+                </Col>
+              </Row>
+            </Col>
+            <Col className="DashboardItem">
+              Current Volume
+            </Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
